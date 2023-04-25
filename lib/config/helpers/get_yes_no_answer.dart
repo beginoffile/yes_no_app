0f6acedca67d084cee0424cc703a6bf09c927a9c
Cesar <cursos.mapatupisaro@gmail.com>
2023-04-25 10:08:39 -0600

    update project

---
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -0,0 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infraestructure/models/yes_no_model.dart';
+
+class GetYesNoAswer {
+  Future<Message> getAnswer() async {
+    final _dio = Dio();
+
+    final response = await _dio.get('https://yesno.wtf/api');
+
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+
+    // return Message(
+    //     text: response.data['answer'],
+    //     fromWho: FromWho.hers,
+    //     imageUrl: response.data['image']);
+
+    return Message(
+        text: yesNoModel.answer,
+        fromWho: FromWho.hers,
+        imageUrl: yesNoModel.image);
+  }
+}
